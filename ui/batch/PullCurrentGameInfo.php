5543561a30f868be70867d39a31625c79ae6f8b4
pol <pol@pol-laptop>
2015-11-01 03:18:45 +0100

    test

--- ui/batch/PullCurrentGameInfo.php
+++ ui/batch/PullCurrentGameInfo.php
@@ -5,12 +5,19 @@ $autoload = require_once __DIR__ . '/../../vendor/autoload.php';
 use Lolnot\Application\Service\NotifyUserWithCurrentGamesService;
 use Lolnot\Infrastructure\Persistence\MySQLDatabase\MySQLSubscriptionRepository;
 use Lolnot\Infrastructure\Persistence\LolApi\LolApiCurrentGameRepository;
+use Lolnot\Infrastructure\Persistence\MySQLDatabase\MySQLChampionRepository;
 use Lolnot\Infrastructure\Mailer\EmailClient;
 
 $subscriptionRepository = new MySQLSubscriptionRepository();
 $currentGameRepository  = new LolApiCurrentGameRepository();
+$championRepository     = new MySQLChampionRepository();
 $mailer                 = new EmailClient();
 
-$service = new NotifyUserWithCurrentGamesService($subscriptionRepository, $currentGameRepository, $mailer);
+$service = new NotifyUserWithCurrentGamesService(
+	$subscriptionRepository,
+	$currentGameRepository,
+	$championRepository,
+	$mailer
+);
 
 $currentGames = $service->execute();
